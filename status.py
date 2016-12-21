af21090d0dea7fba9037414d89ff99049c84eba3
Carwin Zimmermann <carwinz@gmail.com>
2016-12-21 19:42:03 +1100

    clean up curses if the user interrupts the app

--- status.py
+++ status.py
@@ -1,20 +1,19 @@
+import signal
+import sys
 import curses
 from subprocess import call
 from subprocess import check_output
 
-stdscr = curses.initscr()
-
-# Usually curses applications turn off automatic echoing of keys to the screen, in order to be able to read keys and only display them under certain circumstances. This requires calling the noecho() function.
-curses.noecho()
-
-# Applications will also commonly need to react to keys instantly, without requiring the Enter key to be pressed; this is called cbreak mode, as opposed to the usual buffered input mode.
-curses.cbreak()
-
-# Terminals usually return special keys, such as the cursor keys or navigation keys such as Page Up and Home, as a multibyte escape sequence. While you could write your application to expect such sequences and process them accordingly, curses can do it for you, returning a special value such as curses.KEY_LEFT. To get curses to do the job, you'll have to enable keypad mode.
-stdscr.keypad(1)
-
 cursor_line = 0
 status = ''
+
+def signal_handler(signal, frame):
+    exit()
+    sys.exit(0)
+
+def exit():
+    curses.nocbreak(); stdscr.keypad(0); curses.echo()
+    curses.endwin()
 
 def show_status():
     global status
@@ -62,6 +61,19 @@ def ignore():
         ignores.write(line.strip())
     show_status()
 
+stdscr = curses.initscr()
+
+# Usually curses applications turn off automatic echoing of keys to the screen, in order to be able to read keys and only display them under certain circumstances. This requires calling the noecho() function.
+curses.noecho()
+
+# Applications will also commonly need to react to keys instantly, without requiring the Enter key to be pressed; this is called cbreak mode, as opposed to the usual buffered input mode.
+curses.cbreak()
+
+# Terminals usually return special keys, such as the cursor keys or navigation keys such as Page Up and Home, as a multibyte escape sequence. While you could write your application to expect such sequences and process them accordingly, curses can do it for you, returning a special value such as curses.KEY_LEFT. To get curses to do the job, you'll have to enable keypad mode.
+stdscr.keypad(1)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 show_status()
 while 1:
     c = stdscr.getch()
@@ -82,5 +94,4 @@ while 1:
     else:
         show_status()
 
-curses.nocbreak(); stdscr.keypad(0); curses.echo()
-curses.endwin()
+exit()
