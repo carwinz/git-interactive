414469e27a11b0709c2cb5f8da93d40dd184dc89
Carwin Zimmermann <carwinz@gmail.com>
2017-01-16 11:19:17 +1100

    refactor - remove status operations from UI stuff

--- src/status.py
+++ src/status.py
@@ -5,9 +5,9 @@ import sys
 import curses
 from subprocess import call
 from subprocess import check_output
+from status_wrapper import StatusWrapper
 
-cursor_line = 0
-status = ''
+status_wrapper = StatusWrapper()
 
 def signal_handler(signal, frame):
     exit()
@@ -17,98 +17,42 @@ def exit():
     curses.nocbreak(); stdscr.keypad(0); curses.echo()
     curses.endwin()
 
-def move_cursor_to_closest_line():
-    line = current_line()
-    if line == None or not is_line_a_file(line):
-        move_cursor_up()
-        line = current_line()
-    if line == None or not is_line_a_file(line):
-        move_cursor_down()
-
 def show_status():
-    global status
-    status = check_output(["git", "status"])
+    status_wrapper.update_status()
     stdscr.clear()
-    stdscr.addstr(0, 0, status)
+    stdscr.addstr(0, 0, status_wrapper.current_status())
     stdscr.refresh()
-    curses.setsyx(cursor_line, 0)
+    update_cursor()
+
+def update_cursor():
+    curses.setsyx(status_wrapper.selected_line_index, 0)
     curses.doupdate()
-    move_cursor_to_closest_line()
-
-def current_line():
-    global cursor_line
-    global status
-    lines = status.split('\n')
-    if cursor_line + 1 > len(lines):
-        return None
-    return lines[cursor_line]
-
-def move_cursor_up():
-    global cursor_line
-    global status
-    status_lines = status.split('\n')
-    potential_line = cursor_line
-    if potential_line > len(status_lines):
-        potential_line = len(status_lines)-1
-    while potential_line > 0:
-        potential_line = potential_line - 1
-        if is_line_a_file(status_lines[potential_line]):
-            cursor_line = potential_line
-            curses.setsyx(cursor_line, 0)
-            curses.doupdate()
-            break
-
-def move_cursor_down():
-    global cursor_line
-    global status
-    status_lines = status.split('\n')
-    potential_line = cursor_line
-    if potential_line > len(status_lines):
-        potential_line = len(status_lines)-1
-    while potential_line < len(status_lines)-1:
-        potential_line = potential_line + 1
-        if is_line_a_file(status_lines[potential_line]):
-            cursor_line = potential_line
-            curses.setsyx(cursor_line, 0)
-            curses.doupdate()
-            break
-
-def is_line_a_file(line):
-    return line.startswith("	")
-
-def selected_line():
-    return status.split('\n')[cursor_line]
 
 def add():
-    line = selected_line().replace('modified:', '')
-    call(["git", "add", line.strip()])
+    call(["git", "add", status_wrapper.selected_file()])
     show_status()
 
 def checkout():
-    line = selected_line().replace('modified:', '')
-    call(["git", "checkout", line.strip()])
+    call(["git", "checkout", status_wrapper.selected_file()])
     show_status()
 
 def unstage():
-    line = selected_line().replace('modified:', '')
-    call(["git", "reset", "HEAD", line.strip()])
+    call(["git", "reset", "HEAD", status_wrapper.selected_file()])
     show_status()
 
 def ignore():
-    line = selected_line()
     with open(".gitignore", "a") as ignores:
-        ignores.write(line.strip())
+        ignores.write(status_wrapper.selected_file())
     show_status()
 
 def git_rm():
-    line = selected_line().replace('deleted:', '')
+    line = status_wrapper.selected_file()
     call(["git", "rm", line.strip()])
     call(["rm", line.strip()])
     show_status()
 
 def diff():
-    line = selected_line().replace('modified:', '')
-    diff = check_output(["git", "diff", "--staged", line.strip()])
+    diff = check_output(["git", "diff", "--staged", status_wrapper.selected_file()])
     stdscr.clear()
     stdscr.addstr(0, 0, diff)
     stdscr.refresh()
@@ -124,7 +68,6 @@ stdscr.keypad(1) # have curses translate special keys
 signal.signal(signal.SIGINT, signal_handler)
 
 show_status()
-move_cursor_down()
 
 while 1:
     c = stdscr.getch()
@@ -143,9 +86,11 @@ while 1:
     elif c == ord('u'):
         unstage()
     elif c == curses.KEY_UP or c == ord('k'):
-        move_cursor_up()
+        status_wrapper.move_selection_up()
+        update_cursor()
     elif c == curses.KEY_DOWN or c == ord('j'):
-        move_cursor_down()
+        status_wrapper.move_selection_down()
+        update_cursor()
     else:
         show_status()
 
---
+++ src/status_wrapper.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import sys
+from subprocess import call
+from subprocess import check_output
+
+class StatusWrapper():
+
+    selected_line_index = 0
+    status = ''
+    status_lines = []
+
+    def update_status(self):
+        self.status = check_output(["git", "status"])
+        self.status_lines = self._split_status_into_lines()
+        self.move_selection_to_closest_line()
+
+    def move_selection_to_closest_line(self):
+        line = self.current_line()
+        if line == None or not self._is_line_a_file(line):
+            self.move_selection_up()
+            line = self.current_line()
+        if line == None or not self._is_line_a_file(line):
+            self.move_selection_down()
+
+    def current_line(self):
+        if self.selected_line_index + 1 > len(self.status_lines):
+            return None
+        return self.status_lines[self.selected_line_index]
+
+    def move_selection_up(self):
+        potential_line = self.selected_line_index
+        if potential_line > len(self.status_lines):
+            potential_line = len(self.status_lines)-1
+        while potential_line > 0:
+            potential_line = potential_line - 1
+            if self._is_line_a_file(self.status_lines[potential_line]):
+                self.selected_line_index = potential_line
+                break
+
+    def move_selection_down(self):
+        potential_line = self.selected_line_index
+        if potential_line > len(self.status_lines):
+            potential_line = len(self.status_lines)-1
+        while potential_line < len(self.status_lines)-1:
+            potential_line = potential_line + 1
+            if self._is_line_a_file(self.status_lines[potential_line]):
+                self.selected_line_index = potential_line
+                break
+
+    def _split_status_into_lines(self):
+        return self.status.split('\n')
+
+    def _is_line_a_file(self, line):
+        return line.startswith("	")
+
+    def selected_file(self):
+        return self.selected_line().replace('new file:','').replace('modified:', '').replace('deleted:', '').strip()
+
+    def selected_line(self):
+        return self.status_lines[self.selected_line_index]
+
+    def current_status(self):
+        return self.status
